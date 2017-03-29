6760607f319ad1c3e45593781be2a5cdbd2419a4
Sergey Yatsuk <sergeyy@cqg.com>
2017-03-29 18:41:36 +0300

    Init CF407

---
+++ CodeForeces/CF407C/main.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <math.h>
+#include <cassert>
+#include <limits>
+#include <memory.h>
+#include <unordered_map>
+#include <unordered_set>
+
+//#pragma comment(linker,"/STACK:102400000,102400000") // 100 mb
+
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+#define all(c) (c).begin(), (c).end()
+#define CLEAR(x) memset(x,0,sizeof x)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+   //freopen("in.txt", "r", stdin);
+   //freopen("out.txt", "w", stdout);
+
+   int n;
+
+   cin >> n;
+   cout << n;
+
+   return 0;
+}
